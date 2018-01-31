6f360f5989aaa2b028a093cd58d432a56268569f
kevocode <kevindanielguzmen98@gmail.com>
2018-01-31 19:20:09 +0000

    Configuracion BD remota de heroku

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=127.0.0.1;dbname=talento1a',
-    'username' => 'vagrant',
-    'password' => 'vagrant',
+    'dsn' => 'pgsql:host=ec2-54-83-204-230.compute-1.amazonaws.com;dbname=d4d7lvt7ulqqps',
+    'username' => 'psldydfdizcemw',
+    'password' => 'd414dcc8908a71a4c62bed782037926c0ab8b1ea11d197d874cfe2d5ec3ef551',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
