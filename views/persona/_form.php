5d67d79946206385b1b2c872b57c1bb787674e27
kevocode <kevindanielguzmen98@gmail.com>
2018-01-31 21:26:22 +0000

    Modificacion peticiones

--- views/persona/_form.php
+++ views/persona/_form.php
@@ -65,31 +65,42 @@ use yii\helpers\Url;
 
 <?php JSRegister::begin(); ?>
 <script>
-	/* Peticiones ajax */
-	$.ajax({
-		url: '<?= Url::to(['profesiones/index']) ?>',
-		type: 'get',
-		headers: {
-	        'Authorization': 'Bearer ' + '<?= $token ?>'
-	    },
-	    success: function(res) {
-	    	for(profesion in res) {
-	    		$('#personas-profesion').append(`<option value="${res[profesion].profesion_id}">${res[profesion].nombre}</option>`)
-	    	}
-	    }
-	})
-	$.ajax({
-		url: '<?= Url::to(['municipios/index']) ?>',
-		type: 'get',
-		headers: {
-	        'Authorization': 'Bearer ' + '<?= $token ?>'
-	    },
-	    success: function(res) {
-	    	for(municipio in res) {
-	    		$('#personas-municipio').append(`<option value="${res[municipio].municipio_id}">${res[municipio].nombre}</option>`)
-	    	}
-	    }
-	})
+	/* Carga de datos */
+	var linkMunicipios = '<?= Url::to(['municipios/index']) ?>'
+	var linkProfesiones = '<?= Url::to(['profesiones/index']) ?>'
+	cargarDatos('personas-profesion', linkProfesiones, 'profesion_id', 'nombre')
+	cargarDatos('personas-municipio', linkMunicipios, 'municipio_id', 'nombre')
+	/* Cargar los datos */
+	function cargarDatos(idElemento, link, indice, contenido) {
+		/* Consulta de numero de paginas */
+		$.ajax({
+			url: link,
+			type: 'head',
+			headers: {
+		        'Authorization': 'Bearer ' + '<?= $token ?>'
+		    },
+		    success: function(res, textStatus, request) {
+		    	hacerPeticiones(idElemento, request.getResponseHeader('X-Pagination-Page-Count'), link, indice, contenido)
+		    }
+		})
+	}
+	function hacerPeticiones(idElemento, numeroPaginas, link, indice, contenido) {
+		/* Consulta de los datos */
+		for(var i=0; i<numeroPaginas; i++) {
+			$.ajax({
+				url: link + '?page=' + (i+1) + '&per-page=50',
+				type: 'get',
+				headers: {
+			        'Authorization': 'Bearer ' + '<?= $token ?>'
+			    },
+			    success: function(res, textStatus, request) {
+			    	for(dato in res) {
+			    		$('#' + idElemento).append(`<option value="${eval('res[dato].' + indice)}">${eval('res[dato].' + contenido)}</option>`)
+			    	}
+			    }
+			})
+		}
+	}
 </script>
 <?php JSRegister::end(); ?>
 
