17ff8cfae3ee4a3cfe408106ae1c349bf5fc0e9f
kevocode <kevindanielguzmen98@gmail.com>
2018-01-31 01:01:58 +0000

    Correccion inconvenientes filtro

--- views/persona/index.php
+++ views/persona/index.php
@@ -1,13 +1,16 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\grid\GridView;
+use app\models\Profesiones;
+use app\models\Municipios;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\PersonasSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Personas';
+$this->title = 'Administración de personas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="personas-index">
@@ -16,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Personas', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Nuevo', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
@@ -25,10 +28,30 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'persona_id',
+            // 'persona_id',
             'nombre',
-            'profesion',
-            'municipio',
+            // 'profesion',
+            [
+                /* Modificamos los items del grid para mostrar la información exacta */
+                'label' => 'Profesión',
+                'attribute' => 'profesion',
+                'value' => function($model) {
+                    $profesion = Profesiones::findOne($model->profesion);
+                    return $profesion->nombre;
+                },
+                'filter' => ArrayHelper::map(Profesiones::find()->all(), 'profesion_id', 'nombre')
+            ],
+            // 'municipio',
+            [
+                /* Modificamos los items del grid para mostrar la información exacta */
+                'label' => 'Municipio',
+                'attribute' => 'municipio',
+                'value' => function($model) {
+                    $municipio = Municipios::findOne($model->municipio);
+                    return $municipio->nombre;
+                },
+                'filter' => ArrayHelper::map(Municipios::find()->all(), 'municipio_id', 'nombre')
+            ],
             'correo_electronico',
 
             ['class' => 'yii\grid\ActionColumn'],
--- views/persona/view.php
+++ views/persona/view.php
@@ -2,11 +2,13 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\models\Profesiones;
+use app\models\Municipios;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Personas */
 
-$this->title = $model->persona_id;
+$this->title = $model->nombre;
 $this->params['breadcrumbs'][] = ['label' => 'Personas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -15,11 +17,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->persona_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->persona_id], [
+        <?= Html::a('Actualizar', ['update', 'id' => $model->persona_id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Eliminar', ['delete', 'id' => $model->persona_id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Estas seguro de querer eliminar esta persona?',
                 'method' => 'post',
             ],
         ]) ?>
@@ -28,10 +30,20 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'persona_id',
+            // 'persona_id',
             'nombre',
-            'profesion',
-            'municipio',
+            // 'profesion',
+            [
+                'label' => 'Profesión',
+                'attribute' => 'profesion_id',
+                'value' => Profesiones::findOne($model->profesion)->nombre
+            ],
+            // 'municipio',
+            [
+                'label' => 'Municipio',
+                'attribute' => 'municipio_id',
+                'value' => Municipios::findOne($model->municipio)->nombre
+            ],
             'correo_electronico',
         ],
     ]) ?>
