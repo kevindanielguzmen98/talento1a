37acaf3fc21f2fe51cdc71d2474b0b029a18d915
kevocode <kevindanielguzmen98@gmail.com>
2018-02-01 17:34:02 +0000

    Creacion de documentacion de API de controladores

--- controllers/MunicipiosController.php
+++ controllers/MunicipiosController.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
 
 namespace app\controllers;
 
 use yii\filters\auth\HttpBearerAuth;
 
+/**
+ * Component is the base class that provides the *property*, *event* and *behavior* features.
+ *
+ * @include @yii/docs/base-Component.md
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
 class MunicipiosController extends \yii\rest\ActiveController
 {
     public $modelClass = "app\models\Municipios";
